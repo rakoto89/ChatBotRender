6751b346d02a3b5c2b15f9fa9ccafc644761774c
rakoto89 <121390005+rakoto89@users.noreply.github.com>
2025-02-16 18:59:48 -0500

    Update TheChatbotOpioid.py

--- TheChatbotOpioid.py
+++ TheChatbotOpioid.py
@@ -53,5 +53,5 @@ def chat():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    application.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
